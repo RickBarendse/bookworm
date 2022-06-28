be59edff981dd7c2f8c65b089929dc1fd8af9f96
Rick Barendse <dazedeyes@yahoo.com>
2022-06-28 12:06:15 -0700

    commit inital typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+// import the gql template
+const { gql } = require('apollo-server-express');
+
+// create the typeDefs
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+    }
+`
--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const routes = require('./routes');
 
