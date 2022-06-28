46b0b9cd09cb70a10b131d867e202a7844399aa1
Rick Barendse <dazedeyes@yahoo.com>
2022-06-28 13:49:01 -0700

    added more typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,9 +12,32 @@ const typeDefs = gql`
     }
 
     type Book {
-        bookId: ID
+        bookId: ID!
         authors: [String]
         description: String
         title: String
     }
-`
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input SavedBook {
+        authors: [String]
+        description: String
+        bookId: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBook): User
+        removeBook(bookId: String!): User
+    }
+`;
