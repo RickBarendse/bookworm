fd059d2bf41131d4bc0fd210f13ea3b521288bd7
Rick Barendse <dazedeyes@yahoo.com>
2022-06-29 12:46:53 -0700

    fixed graphql connection error

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ const { typeDefs, resolvers } = require('./schemas');
 // const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-//const { Server } = require('http');
+// const { Server } = require('http');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Create new Apollo server and pass in our schema data
@@ -22,7 +22,9 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -34,8 +36,10 @@ if (process.env.NODE_ENV === 'production') {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
 ;
-  
-server.applyMiddleware({ app });
+
+const startApolloServer = async (typeDefs, resolvers ) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 
 // app.use(routes);
@@ -44,9 +48,10 @@ db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`🌍 Now listening on localhost:${PORT}`);
     console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+    });
+  })
+};
 
 
 // Call the async function to start the server
-// startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
