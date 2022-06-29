bca594b059e3aa1150dea85fc691dc0515b6ebea
Rick Barendse <dazedeyes@yahoo.com>
2022-06-28 17:56:30 -0700

    add mutations for login, add user and save book to front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }    
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: SavedBooks) {
+        saveBook(book: $book) {
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                authors
+                description
+            }
+        }
+    }
+`;
