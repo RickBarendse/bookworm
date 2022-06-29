0c2f4f5477659a7c94f07a95eb648d8e959a0cb3
Rick Barendse <dazedeyes@yahoo.com>
2022-06-28 18:10:21 -0700

    updates to App.js to include Apollo client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,10 +2,12 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
+        login(email: $email, passworkd: $password) {
         token
         user {
             _id
             username
+            }
         }
     }
 `;
