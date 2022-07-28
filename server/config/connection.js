257712d3546cff303cdfc1589a7e553abf42a282
Rick Barendse <dazedeyes@yahoo.com>
2022-07-28 16:51:35 -0700

    updating connection configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //update local connection string
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookworm', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookwormdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
