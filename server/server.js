4e8beb44f2be5d7acc75a4d734cc4831b0e93b61
Rick Barendse <dazedeyes@yahoo.com>
2022-07-28 11:18:54 -0700

    change port"

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 // Create new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
