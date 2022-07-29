e667e72117a54c61138ffc7ac70cf9a4b85d6b01
Rick Barendse <dazedeyes@yahoo.com>
2022-07-28 17:59:40 -0700

    install apollo express

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //update local connection string
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookwormdb', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookworm', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
